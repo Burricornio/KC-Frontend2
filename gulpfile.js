a20ac901ff3daff20cd6d7389c37a914246d06bd
Burricornio <alejandro_web@hotmail.com>
2017-08-19 12:16:16 +0200

    Añadimos notificaciones a las tareas de gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require("gulp"); // importamos la librería gulp
-var sass = require("gulp-sass");
+var sass = require("gulp-sass"); // Copilacionde sass
+var notify = require("gulp-notify"); // notificaciones
 var browserSync = require("browser-sync").create();
 
 // definimos la tarea por defecto
@@ -12,13 +13,19 @@ gulp.task("default", function(){
     gulp.watch(["src/scss/*.scss", "src/scss/**/*.scss"], ["sass"]);
 
     //Observa cambios en los archivos HTML y entonces carga el navegador 
-    gulp.watch("src/*.html").on("change", browserSync.reload);
+    gulp.watch("src/*.html", function(){
+        browserSync.reload();
+        notify().write("HTML rulando a topete 👻");
+    });
 });
 
 // compilar sass
 gulp.task("sass", function(){
     gulp.src("src/scss/style.scss") // cargamos el archivo style.scss
-        .pipe(sass().on("error", sass.logError)) // lo compilamos con gulp-sass
+        .pipe(sass().on("error", function(error){
+            return notify().write(error);
+        })) // lo compilamos con gulp-sass
         .pipe(gulp.dest("src/css/")) // guardamos el resultado en la carpeta css
-        .pipe(browserSync.stream()); // recargue el CSS del navegador
+        .pipe(browserSync.stream()) // recargue el CSS del navegador
+        .pipe(notify("SASS compilado 🙌")); // Muestra notificación al actualizas SASS
 });
